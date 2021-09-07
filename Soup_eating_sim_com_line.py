e262d86cdaa8b8966b30a01c1dd8768c26a27204
DomnCostel <75140726+DomnCostel@users.noreply.github.com>
2021-09-07 16:32:27 +0300

    Update Soup_eating_sim_com_line.py

--- Soup_eating_sim_com_line.py
+++ Soup_eating_sim_com_line.py
@@ -34,9 +34,10 @@ def die():
   print("                                     THE END.(end 1 of ?)                           ")
 def live():
   print("The soup was initially poisoned, like all soup is, but the salt neutralized it so you lived")
-  print("                                     THE END.(end 2 of ?)                                  ")
+  print("                                THE END.(end 2 of ?)                                       ")
 def saltydeath():
   print("you ate way too much salt and died a salty death. you ascend into the Eternal sea, meet Crab God, and rave till the end of time")
+  print("                                                   THE END.(end 3 of ?)                                                        ")  
 #the eating
 print("Eat the soup.")
 print(soup)
@@ -57,7 +58,12 @@ while answer1 not in soupyanswerswithnosoup: #loop of soup
         if salty <= 9:
         eatsoup()
         die()
-        if salty >=9 and salty <=19          
+        if salty >=9 and salty <=19:
+        live()
+        if salty >=20 and salty != 53:
+        saltydeath()
+        else:
+         print("you dummie, im just asking for a number, is that too hard for you?") 
        else:
          soupeaten = True
 
